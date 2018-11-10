15c4279fa542567147fb92e32cd458b1acd3712e
eternity <30304096+0011nj@users.noreply.github.com>
2018-11-10 23:16:11 +0800

    Add files via upload

---
+++ xiao/js/data.js
@@ -0,0 +1,173 @@
+var data = [];
+var dataStr = '1、<br>\
+<br>\
+·<i>hello 交一下申请表</i><br>\
+·<i><p style="float:right">好的,放这里吧。</p></i><br>\
+·<i><p style="float:right">党春晓,这名字听着还熟悉呀,还是我们哪里的,你认识嘛</p></i><br>\
+·<i><p style="float:right">不认识,人家比我们小两届了,你认识啥呀</p></i><br>\
+·<i><p style="float:right">好吧,可是我感觉好熟悉呀</p></i><br>\
+<br>\
+<br>\
+2、<br>\
+<br>\
+·<i><p style="float:right">(坏了,她没加我们群呀)</p></i><br>\
+·<i>qq好友申请~~di~~di</i><br>\
+·<i><p style="float:right">你是?</p></i><br>\
+·<i></i>我是大一的党春晓<br>\
+·<i><p style="float:right">(哇,这不是那个嘛)</p></i><br>\
+·<i><p style="float:right">不好意思呀,那天不再摊位上,没通知你加群,下周训练</p></i><br>\
+<br>\
+<br>\
+3、<br>\
+<br>\
+·<i><p style="float:right">(第一次训练)</p></i><br>\
+·<i><p style="float:right">喂,你这个要这样弄</p></i><br>\
+·<i><br>\你不要过来了</i><br>\
+·<i><p style="float:right">(嘿,这娃咋还不让教呢)额,好吧</p></i><br>\
+·<i><p style="float:right">(哎,算了算了,还有点小脾气)</p></i><br>\
+·<i><p style="float:right">(还觉得我影响她了)</p></i><br>\
+·<i>这是谁呀?</i><br>\
+·<i>学长,冀祥</i><br>\
+·<br>\
+<br>\
+<br>\
+4、<br>\
+<br>\
+·<i><p style="float:right">(我去上个厕所,你带他们)</p></i><br>\
+·<i>累死了,歇会儿吧</i><br>\
+·<i><p style="float:right">才多大一会儿嘛就累了</p></i><br>\
+·<i>哎呀,你看他走了,歇会儿</i><br>\
+·<i><p style="float:right">哎,行行行,休息休息吧</p></i><br>\
+·<i><p style="float:right">(我叫你给他们训练,你咋让他们休息了)</p></i><br>\
+<br>\
+<br>\
+5、<br>\
+<br>\
+·<i>QQ~~di~~di</i><br>\
+·<i>学长,我报新生杯了,你能陪我打打球嘛</i><br>\
+·<br>\
+·<i><p style="float:right">可以吧,那你等会儿看去东门吧</p></i><br>\
+·<i>嗯嗯,好的</i><br>\
+·<i><p style="float:right">不好意思,久等了</p></i><br>\
+·<i>嘿嘿,没事</i><br>\
+<br>\
+<br>\
+6、<br>\
+<br>\
+·<i><p style="float:right">(新生杯比赛结束)</p></i><br>\
+·<i><p style="float:right">你今天怎么了,不高兴嘛</p></i><br>\
+·<i><br>\没有呀,咋了</i><br>\
+·<i><p style="float:right">我看你打完也不理人家的就走了</p></i><br>\
+·<i>我不知道呀,你们也没说过</i><br>\
+·<i><p style="float:right">额,好吧,好像真没教过</p></i><br>\
+·<i>我都不知道干啥,好尴尬</i><br>\
+<br>\
+<br>\
+7、<br>\
+<br>\
+·<i>(不知什么时候我的称呼就成大神了)</i><br>\
+·<i><p style="float:right">我英语就三十分左右</p></i><br>\
+·<i>大神,那我给你补习英语吧</i><br>\
+·<i><p style="float:right">不是吧,你别逗我</p></i><br>\
+·<i>我哄你干啥,说真的,就当我自已也复习了</i><br>\
+·<i><p style="float:right">我靠,还真来呀,那你不嫌麻烦那我咋好意思不答应呢</p></i><br>\
+·<i>行,那你就把这些英标学一下,我下周检查</i><br>\
+·<i><p style="float:right">这么快</p></i><br>\
+<br>\
+<br>\
+8、<br>\
+<br>\
+·<i>(验收英标)</i><br>\
+·<i>你等会儿,他们叫我去开个会</i><br>\
+·<i><p style="float:right">额,咋又推到你身上了?,算了那还是我去吧</p></i><br>\
+·<i>你咋啥没拿,给你个本子进去</i><br>\
+·<i><p style="float:right">(看看本子,她还真从英标学了一遍)</p></i><br>\
+·<i><p style="float:right">你先回去吧,看样子这会还得好一会呢</p></i><br>\
+·<i>没事,你开你的,我等你</i><br>\
+·<i><p style="float:right">哎,这么冷的,你待这,走吧吃饭</p></i><br>\
+·<i>来检查吧</i><br>\
+·<i><p style="float:right">还记着呢,来来来shan</p></i><br>\
+<br>\
+<br>\
+9、<br>\
+<br>\
+·<i><p style="float:right">放假干嘛呀?</p></i><br>\
+·<i>没事干呀,待宿舍</i><br>\
+·<i><p style="float:right">这么宅,引你出去玩嘛</p></i><br>\
+·<i>哎,那走吧</i><br>\
+·<i><p style="float:right">想去哪</p></i><br>\
+·<i>不知道,听你安排呗,嘿嘿</i><br>\
+·<i><p style="float:right">(- -- -- -- -- -后来我们就常常一起出去走走逛逛了)</p></i><br>\
+<br>\
+<br>\
+10、<br>\
+<br>\
+·<i>大神,你以后要去哪里呀</i><br>\
+·<i><p style="float:right">不知道呀,但还是想出去看看吧</p></i><br>\
+·<i>那也挺好的</i><br>\
+·<i><p style="float:right">怎么你要和我一起嘛<斜眼笑><斜眼笑></p></i><br>\
+·<i>对呀,黏上你了</i><br>\
+·<i><p style="float:right">哇,这么好嘛</p></i><br>\
+<br>\
+<br>\
+11、<br>\
+<br>\
+·<i>大神,你如果要是有喜欢的人了,给我说一声,我就会离开</i><br>\
+·<i><p style="float:right">没有呀(好像有了,就是你呀)</p></i><br>\
+·<i><br>\大神这么好的,我可不舍得拱手相让</i><br>\
+·<i><p style="float:right"><br>\怎么不拱手,那是要一脚踹出去了</p><br>\</i><br>\
+·<i>嘿嘿,不会不会</i><br>\
+<br>\
+<br>\
+12、<br>\
+<br>\
+·<i><p style="float:right">你们怎么放假那么迟呀</p></i><br>\
+·<i>不知道,就等这几天玩考个试就结束了</i><br>\
+·<i><p style="float:right">我明回家,过几天出来接你</p></i><br>\
+·<i>嘿嘿,真的呀</i><br>\
+·<i><p style="float:right">那当然呀</p></i><br>\
+·<i></i><br>\
+·<i><p style="float:right">(还记得你靠在我肩膀上的样子,做我女朋友吧)</p></i><br>\
+·<i>(好呀)</i><br>\
+<br>\
+<br>\
+13、生日快乐<br>\
+<br>\
+·<i></i><br>\
+·<i></i><br>\
+·<i></i><br>\
+·<p style="text-align:center">晓儿</p><p style="text-align:center">谢谢你的出现</p><p style="text-align:center">如果说余生都是你</p><p style="text-align:center">晚一点有什么关系</p><p style="text-align:center">嗨</p><p style="text-align:center">生日快乐</p><br>\
+·<br>\
+·<br>\
+·<br>\
+<br>\
+<br>\
+14、<br>\
+<br>\
+·我长成这样是为了让人看起来更想拥抱。<br>\
+·如果你生气了，就欺负我好了，反正我那么喜欢你。<br>\
+·相信我，就算有一天，我们不小心走散，我也会回来找你的。<br>\
+·为了你，我什么都会去做。只是有时候我笨手笨脚的，能不能不要怪我呢。<br>\
+·我们有时并不想成为英雄，只是生活有时候超出我们的想象。<br>\
+<br>\
+<br>\
+20、<br>\
+<br>\
+·当人们做不到一些事情的时候，他们就会对你说你也同样不能。<br>\
+·如果你有梦想的话，就要去捍卫它。<br>\
+·当你最认为困难的时候，其实就是你最接近成功的时候。<br>\
+·以后连朋友都不做了？ ---没法儿做，太相爱的人怎么做朋友<br>\
+·机会总是留给有准备的人，但那往往是努力的人剩下来的。<br>\
+';
+var d = dataStr.split('<br><br><br>');
+//console.log(d)
+for (var i = 0; i < 13; i++) {
+    var c = d[i].split('<br><br>');
+	//console.log(c)
+    data.push({
+        img: c[0].replace('、', ' ') + '.jpg',
+        caption: c[0].split('、')[1],
+        desc: c[1]
+    });
+    //console.log(c[0].replace('、', ' ') + '.jpg');
+};
